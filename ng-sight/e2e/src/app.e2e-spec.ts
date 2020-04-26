72703d3920962628a59770b3b2699d05b935c310
Fatih Koç <fkoc.041@gmail.com>
2020-04-26 19:12:55 +0300

    Frontend project name change

--- ng-sight/e2e/src/app.e2e-spec.ts
+++ ng-sight/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('ngd-frontend app is running!');
+    expect(page.getTitleText()).toEqual('ng-sight app is running!');
   });
 
   afterEach(async () => {
--- ng-sight/src/app/app.component.spec.ts
+++ ng-sight/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ngd-frontend'`, () => {
+  it(`should have as title 'ng-sight'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ngd-frontend');
+    expect(app.title).toEqual('ng-sight');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ngd-frontend app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('ng-sight app is running!');
   });
 });
